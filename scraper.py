93d3f2deb7b4701fdff0bea3c77dc04b1c102bd2
BrandonHoeft <brandonmhoeft@gmail.com>
2018-06-06 22:02:55 -0500

    in which our hero updates the batter URL to be a concatenated string for looping over all 30 teams.

--- scraper.py
+++ scraper.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-batter_url = "https://www.fangraphs.com/dailyprojections.aspx?pos=all&stats=bat&type=sabersim"
-pitcher_url = "https://www.fangraphs.com/dailyprojections.aspx?pos=all&stats=pit&type=sabersim"
+team_id = 1
+team_batters_url = "https://www.fangraphs.com/dailyprojections.aspx?pos=all&stats=bat&type=sabersim&team=" + str(team_id)
+all_pitcher_url = "https://www.fangraphs.com/dailyprojections.aspx?pos=all&stats=pit&type=sabersim"
 
 # package the GET request, send & catch response
-r = requests.get(batter_url)
+r = requests.get(team_batters_url)
 
 # extract response HTML structure
 html_doc = r.text
@@ -22,4 +23,7 @@ tag_soup.get_text()
 # PLACEHOLDER: Note that I may need to incorporate pagination, complex parsing.
 # https://bit.ly/2LiUSTN
 # https://github.com/bwainstock/baseball-scraper
+
+
+
 
